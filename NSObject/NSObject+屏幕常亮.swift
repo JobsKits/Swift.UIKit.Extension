5e079c3b3c327202b118a9b154fc1a3a3083f763
Jobs <lg295060456@gmail.com>
2025-12-09 14:10:46 +0700

    no message

---
+++ NSObject/NSObject+屏幕常亮.swift
@@ -0,0 +1,29 @@
+//
+//  NSObject+屏幕常亮.swift
+//  JobsSwiftBaseConfigDemo
+//
+//  Created by Jobs on 12/9/25.
+//
+#if os(OSX)
+    import AppKit
+#endif
+
+#if os(iOS) || os(tvOS)
+    import UIKit
+#endif
+// MARK: - 屏幕常亮
+public extension NSObject {
+    /// 屏幕是否保持常亮
+    var isScreenAlwaysOn: Bool {
+        get { UIApplication.shared.isIdleTimerDisabled }
+        set { UIApplication.shared.isIdleTimerDisabled = newValue }
+    }
+    /// 开启常亮
+    func keepScreenOn() {
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    /// 关闭常亮
+    func endScreenOn() {
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+}
